cfef7238e117352c11cd634426b6a275be82308c
ackahpaul <ackahpaul@gmail.com>
2022-11-12 08:28:01 -0700

    update files

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import Pages from './pages';
+import './App.css';
+import React,{useState,useEffect} from 'react';
+
+
+
+
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // NOTE: Use your username below
+    fetch('https://gitconnected.com/v1/portfolio/paulackah')
+      .then(res => res.json())
+      .then(user => {
+        setUser(user);
+      });
+  }, []);
+
+  if (!user) {
+    return <div />;
+  }
+
+  return <Pages user={user} />;
+}
+export default App;
